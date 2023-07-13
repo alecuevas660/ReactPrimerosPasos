ff6e177f8ebcaefa34f2210cd4b70acbe4502ab8
alecuevas660 <alecuevas660@gmail.com>
2023-07-13 17:48:50 -0400

    Actualización

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>3 clasicos para recordar</h1>
-        <Game />
+        <Game
+        nombre='Assassins Creed Black Flag'
+        fecha= '29 de octubre de 2013'
+        descripcion = 'El escenario es ahora el mar Caribe, y la época principios del siglo XVIII, con un Edward Kenway que ha decidido dedicarse a la piratería por verle mayor ganancia que ser un corsario al servicio de la marina inglesa. Una serie de acontecimientos le irán sumergiendo en la eterna guerra entre los asesinos y templarios mientras cumplimos todo tipo de misiones a lo largo y ancho del Caribe en su época más memorable. Si en ACIII contábamos con un barco y un sistema de batallas navales un tanto paralelo y opcional, en ACIV: Black Flag es una de las partes centrales del juego. A bordo de nuestro barco Jackdaw surcaremos las aguas más calientes del mundo convirtiéndonos en el terror de los mares, en misiones que combinan esta dinámica con las mecánicas clásicas de Assassin’s Creed: Sigilo, armas e ítems para aprovecharlo, y un sistema de combate sencillo pero muy satisfactorio.'
+        imagen='ac'
+        />
+
+        <Game
+        nombre='God Of war'
+        fecha= '22 de marzo de 2005'
+        descripcion = 'Kratos es el protagonista de God of War, un feroz guerrero espartano que es, a su vez, hijo de Zeus y Calisto, lo que le otorga el estatus de semidiós. El relato real del personaje, por otra parte, comienzo cuando hace un trato con Ares, el dios griego de la guerra: si accede a destruir a sus enemigos, Kratos le ofrecerá su vida como sirviente. Ares, lógicamente, acepta ante la posibilidad de hacerse con un esbirro más.'
+        imagen='gow'/>
+
+        <Game
+        nombre='Prince Of Persia'
+        fecha= '22 de marzo de 2005'
+        descripcion = 'En el medio oriente, un príncipe persa se alía con la princesa de un reino vecino para proteger el secreto de las Arenas del Tiempo y también para capturar al asesino del rey de Persia, el cual pretende retroceder en el tiempo.'
+        imagen='prince'/>
       </div>
     </div>
   );
--- src/componentes/game.js
+++ src/componentes/game.js
@@ -3,17 +3,17 @@ import '../estilos/game.css';
 
 //Definir componente funcional
 
-export function Game(){
+export function Game(props){
   return(  //se retornara todo el codigo a la función Game  
     <div className='contenedor-juego'>    
       <img
        className='imagen-juego'
-       src={require('../img/acblackflag.jpg')} alt='acbf'/> 
+       src={require(`../img/reseña-${props.imagen}.jpg`)} alt='imggame'/> 
 
        <div className='contenedor-reseña'>
-        <p className='nombre-juego'>Assasin's Creed's Black Flag</p>
-        <p className='fecha-juego'>29 de octubre de 2013</p>
-        <p className='descripcion-juego'>Todos tenemos nuestras películas sobre combates absurdos. Freddy contra Jason. Mega Tiburón contra Pulpo Gigante. Pero cuando se trata de Piratas contra Assassin's Creed, afortunadamente Black Flag es mucho más que una escaramuza. Este viaje por el Caribe con el pícaro Edward Kenway es una gloriosa aventura empapada de ron. Pistolas, espadas, cañones, islas desiertas y los famosos piratas de la época hacen que el nacimiento de Edward como Assassin se sienta como un sueño brillante. Ignora la campaña, zarpa y averigua a dónde te lleva el viento. Aventuras en una campana de buceo, caza de ballenas, tesoros enterrados, emborracharse en bares, todo está aquí. Y aniquilar barco tras barco y reclamar su tripulación y sus tesoros es más que gratificante.</p>
+        <p className='nombre-juego'>{props.nombre}</p>
+        <p className='fecha-juego'>{props.fecha}</p>
+        <p className='descripcion-juego'>{props.descripcion}</p>
        </div>
     </div>
 
